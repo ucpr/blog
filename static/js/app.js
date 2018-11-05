86810dc11eff2b8e794529fbac1cb5d2e142ed2c
nve3pd <uchihara1229@gmail.com>
2018-11-05 18:14:35 +0000

    [add] initial commit

---
+++ static/js/app.js
@@ -0,0 +1,10 @@
+function init() {
+    document.getElementById("about").style.display = "none";
+    document.getElementById("works").style.display = "none";
+    document.getElementById("events").style.display = "none";
+    document.getElementById("socials").style.display = "none";
+}
+
+init();
+
+document.getElementById("about").style.display = "";
