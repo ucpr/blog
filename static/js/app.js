18b49379e40a1b2b0925a4be564f967015abaa7b
nve3pd <uchihara1229@gmail.com>
2018-11-06 08:17:58 +0000

    [feat] ul liで押されたやつが表示されるようにした

--- static/js/app.js
+++ static/js/app.js
@@ -1,10 +1,34 @@
 function init() {
-    document.getElementById("about").style.display = "none";
-    document.getElementById("works").style.display = "none";
-    document.getElementById("events").style.display = "none";
-    document.getElementById("socials").style.display = "none";
+    $("#about").hide();
+    $("#works").hide();
+    $("#events").hide();
+    $("#socials").hide();
 }
 
-init();
+$(function(){
+    init();
+    $("#about").css("display", "");
 
-document.getElementById("about").style.display = "";
+    $("nav ul li").click(function() {
+        // ul liで押された場所の取得
+        var index = $(this).index() + 1;
+        switch (index) {
+            case 1:
+                init();
+                $("#about").css("display", "");
+                break;
+            case 2:
+                init();
+                $("#works").css("display", "");
+                break;
+            case 3:
+                init();
+                $("#events").css("display", "");
+                break;
+            case 4:
+                init();
+                $("#socials").css("display", "");
+                break;
+        }
+    });
+});
